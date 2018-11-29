81106eb1ec57eb19e5f1295f20eb7cd16e2575ce
Your Name <jawadalikhel18@gmail.com>
2018-11-29 14:54:36 -0600

    created the login and register form

--- health-track-app/src/HealthComponent/Login/index.js
+++ health-track-app/src/HealthComponent/Login/index.js
@@ -5,7 +5,18 @@ class Login extends Component {
     return (
       <div className="App">
         <h1>Login</h1>
+        <form>
+          <input type="text" placeholder="username" />
+          <input type="password" placeholder="password" />
+          <button>Login</button>
+        </form>
 
+        <h1>Register</h1>
+        <form>
+          <input type="text" placeholder="username" />
+          <input type="password" placeholder="password" />
+          <button>Register</button>
+        </form>
       </div>
     );
   }
