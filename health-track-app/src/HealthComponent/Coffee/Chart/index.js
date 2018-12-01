3dc6bad9b8f090eddfaccebb04373a3141c6f76b
Your Name <jawadalikhel18@gmail.com>
2018-11-30 23:29:31 -0600

    displayed the chart and its showing now the usual amount of coffe the user drinks

--- health-track-app/src/HealthComponent/Coffee/Chart/index.js
+++ health-track-app/src/HealthComponent/Coffee/Chart/index.js
@@ -1,10 +1,35 @@
 import React, {Component} from 'react';
+import {Bar, Line, Pie} from 'react-chartjs-2';
 
 class Chart extends Component{
+  constructor(){
+  super();
+  this.state = {
+    loading: true,
+    chartData:{
+      labels: ['past', 'daily'],
+      datasets: [
+        {
+          label: 'COFFEE',
+          data: [
+              20,
+              0,
+              0,
+          ],
+          backgroundColor:[
+            'rgba(255,99,132,0.6)'            ]
+          }
+        ]
+      }
+    }
+  }
   render(){
     return(
       <div>
         <h1>Chart</h1>
+        <Bar
+          data={this.state.chartData}
+        />
       </div>
     )
   }
