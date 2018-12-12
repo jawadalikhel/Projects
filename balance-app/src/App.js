622b68b629b3934a29ce9e6bf38992622c49cc82
Your Name <jawadalikhel18@gmail.com>
2018-12-12 12:53:11 -0600

    starting developing the balance app

---
+++ balance-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>BALANCE APP</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
