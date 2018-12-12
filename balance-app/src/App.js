fa3e2082c613cd63dbb00c41affacad090bff994
Your Name <jawadalikhel18@gmail.com>
2018-12-12 13:09:49 -0600

    created the main component and connected it with the app component

--- balance-app/src/App.js
+++ balance-app/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Main from './MainComponent';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>BALANCE APP</h1>
+        <Main />
       </div>
     );
   }
---
+++ balance-app/src/MainComponent/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './style.css';
+class Main extends Component{
+  render(){
+    return(
+      <div id="MainContainer">
+        <h1>BAlance Main</h1>
+      </div>
+    )
+  }
+}
+
+export default Main;
